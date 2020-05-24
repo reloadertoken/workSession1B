f5ee8e53f0e3ed09a32323e60bf8dfe54d68ba97
Reloader <suarezfc82@gmail.com>
2020-05-24 16:15:52 -0500

    Initial Commit

---
+++ SessionOneB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SessionOneB
+//
+//  Created by Resembrink Correa on 5/24/20.
+//  Copyright © 2020 Reloader. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
